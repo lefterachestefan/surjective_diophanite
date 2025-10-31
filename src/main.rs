8aa21611235754c1cd24fcc8799adeb1088e8c5b
Dazor Plasma <dazorplasma@proton.me>
2025-10-31 12:37:36 +0200

    removed const

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 use binary_search::{Direction, binary_search};
 use rayon::prelude::*;
-use rug::{Complete, Integer};
 use std::time::Instant;
 
-const CHECK_UP_TO: usize = 10000;
-const CAP: usize = 20032;
+const CHECK_UP_TO: usize = 3000;
+type SurjectiveLine = [u64; CHECK_UP_TO + 1];
+
+const BIG_PRIME: u64 = 12345678910987654321u64;
 
 #[inline(always)]
-fn next_m(prev: &mut [Integer; CHECK_UP_TO + 1], m: usize) {
-    let mut last: Integer = Integer::with_capacity(CAP);
-    for n in 1..=m {
+fn next_m(prev: &mut SurjectiveLine, m: usize) {
+    let mut last = 1u64;
+    for n in 2..=m {
         unsafe {
-            let new_last = std::mem::take(prev.get_unchecked_mut(n));
-            *prev.get_unchecked_mut(n) = n * (last + &new_last);
-            // dbg!(&new_last.capacity());
-            last = new_last;
+            let new = n as u64 * (last + prev.get_unchecked(n)) % BIG_PRIME;
+            last = std::mem::replace(prev.get_unchecked_mut(n), new);
         }
     }
+}
+
+#[inline(always)]
+fn get_start_line() -> SurjectiveLine {
+    let mut surjective: [u64; CHECK_UP_TO + 1] = const { [0u64; CHECK_UP_TO + 1] };
+    unsafe { *surjective.get_unchecked_mut(1) = 1 };
+    surjective
 }
 
 fn main() {
     let start_time = Instant::now();
     let mut last_check_time = start_time;
-    let mut surjective: [Integer; CHECK_UP_TO + 1] =
-        std::array::from_fn(|_| Integer::with_capacity(CAP));
-    surjective[1] = Integer::from(1);
+    let mut surjective = get_start_line();
     for m in 2..=CHECK_UP_TO {
         next_m(&mut surjective, m);
         let change_point = binary_search((1, ()), (m - 1, ()), |i| unsafe {
@@ -43,12 +47,12 @@ fn main() {
             let (left_result, right_result) = rayon::join(
                 || {
                     surjective[1..=change_point]
-                        .binary_search_by(|i| (x_side + i).complete().cmp(right_side))
+                        .binary_search_by(|i| (x_side + i).cmp(right_side))
                         .is_ok()
                 },
                 || {
                     surjective[change_point + 1..=m]
-                        .binary_search_by(|i| (x_side + i).complete().cmp(right_side).reverse())
+                        .binary_search_by(|i| (x_side + i).cmp(right_side).reverse())
                         .is_ok()
                 },
             );
@@ -69,4 +73,17 @@ fn main() {
         "Program checked up to {CHECK_UP_TO} in {:.2} seconds.",
         start_time.elapsed().as_secs_f64()
     );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn gen_lines() {
+        let mut line = get_start_line();
+        for i in 2..=CHECK_UP_TO {
+            next_m(&mut line, i);
+        }
+        println!("{}", size_of_val(&line));
+    }
 }
